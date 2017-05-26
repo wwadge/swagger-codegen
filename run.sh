4f01f64f9349e5c9ee8238514f9daf02f1e681c2
Wallace Wadge <wwadge@gmail.com>
2017-05-26 11:57:58 +0200

    Renamed json to yml

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -jar ./modules/swagger-codegen-cli/target/swagger-codegen-cli.jar  generate -i friends.json --import-mappings EntityId=com.friends.transactionhandler.util.EntityId -c ./generator-config.json  -l spring -t ./modules/swagger-codegen/src/main/resources/JavaSpring/ -o /Users/wwadge/Documents/workspace/eft/friends-transaction-manager
+java -jar ./modules/swagger-codegen-cli/target/swagger-codegen-cli.jar  generate -i friends.yml --import-mappings EntityId=com.friends.transactionhandler.util.EntityId -c ./generator-config.json  -l spring -t ./modules/swagger-codegen/src/main/resources/JavaSpring/ -o /Users/wwadge/Documents/workspace/eft/friends-transaction-manager
 
