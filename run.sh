7f5ca1d4c880b1867df76126322cc62c9e596705
Waylon Mifsud <waylon@eftsoftware.com>
2017-05-31 11:09:06 +0200

    Updated apiDelegate with isTransactionHandler check

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -DdebugOperations2 -jar ./modules/swagger-codegen-cli/target/swagger-codegen-cli.jar  generate -i friends.yml --import-mappings MonetaryAmount=javax.money.MonetaryAmount,EntityId=com.friends.transactionhandler.util.EntityId -c ./generator-config.json  -l spring -t ./modules/swagger-codegen/src/main/resources/JavaSpring/ -o /tmp/test
+java -DdebugOperations2 -jar ./modules/swagger-codegen-cli/target/swagger-codegen-cli.jar  generate -i ~/Documents/friends-transaction-manager-backend-api/friends.yml --import-mappings MonetaryAmount=javax.money.MonetaryAmount,EntityId=com.friends.transactionhandler.util.EntityId -c ./generator-config.json  -l spring -t ./modules/swagger-codegen/src/main/resources/JavaSpring/ -o ~/Documents/swagger-codegen/friends-transaction-handler
 
